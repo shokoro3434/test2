513aba0d6c7322381d320a27a28aa7749e652919
takahiro <takahiro@ThinkPadX201>
2015-09-03 06:58:58 +0900

    a

--- eitax-rest/src/main/java/com/eitax/recall/yahoo/api/MarkRestController.java
+++ eitax-rest/src/main/java/com/eitax/recall/yahoo/api/MarkRestController.java
@@ -3,6 +3,9 @@ package com.eitax.recall.yahoo.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,16 +24,16 @@ public class MarkRestController {
 	@Autowired
 	private YahooServiceFacade yahooServiceFacade;
 	
-	@RequestMapping(value="{yahooAuctionItemId}",method = RequestMethod.GET,produces = "application/json")
-	public Result putMark(@PathVariable Integer yahooAuctionItemId, @RequestParam Integer markId,UriComponentsBuilder uriComponentsBuilder) {
+	@RequestMapping(value="{yahooAuctionItemId}",method = RequestMethod.PUT,produces = "application/json")
+	public ResponseEntity<Result> putMark(@PathVariable Integer yahooAuctionItemId, @RequestParam Integer markId,UriComponentsBuilder uriComponentsBuilder) {
 		System.out.println(yahooAuctionItemId);
 		log.info(yahooAuctionItemId.toString());
 		Result r = new Result();
 		int ret = this.yahooServiceFacade.updateAuctionItem(yahooAuctionItemId, markId);
 		r.setCode(ret);
-//		HttpHeaders headers = new HttpHeaders();
-		//headers.setLocation(uriComponentsBuilder.path("/hoges/{id}").buildAndExpand(store.size()).toUri());
-		//return new ResponseEntity<String>(data, headers, HttpStatus.CREATED);
-		return r;
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Access-Control-Allow-Origin: ", "");
+//		headers..setLocation(uriComponentsBuilder.path("/hoges/{id}").buildAndExpand(store.size()).toUri());
+		return new ResponseEntity<Result>(r, headers, HttpStatus.CREATED);
 	}
 }
