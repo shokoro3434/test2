aab53d5ff935829967c10519fa88049544637550
takahiro <takahiro@ThinkPadX201>
2015-09-10 02:24:12 +0900

    a

--- eitax-batch/src/main/java/com/eitax/recall/yahoo/repository/YahooAuctionItemRepository.java
+++ eitax-batch/src/main/java/com/eitax/recall/yahoo/repository/YahooAuctionItemRepository.java
@@ -17,7 +17,7 @@ public interface YahooAuctionItemRepository extends JpaRepository<YahooAuctionIt
     @Query(value = "update recall.t_yahoo_auction_item set mark_id = :markId ,updated = sysdate where yahoo_auction_item_id = :yahooAuctionItemId",nativeQuery=true)
     public abstract int updateMarkId(@Param("markId")Integer markId,@Param("yahooAuctionItemId")Integer yahooAuctionItemId);
 
-    @Query(value = "select * from recall.t_yahoo_auction_item where notified = 0 and bid_or_buy <= :bidOrBuy and end_time >= TO_CHAR((sysdate-30/3600),'YYYY-MM-DD\"T\"HH24:MI:SS') order by end_time asc",nativeQuery=true)
+    @Query(value = "select * from recall.t_yahoo_auction_item where notified = 0 and (bid_or_buy <> 0 and bid_or_buy <= :bidOrBuy) and end_time >= TO_CHAR((sysdate-30/3600),'YYYY-MM-DD\"T\"HH24:MI:SS') order by end_time asc",nativeQuery=true)
     public abstract List<YahooAuctionItem> findByBidOrBuy(@Param("bidOrBuy")Integer bidOrBuy);
 
     @Modifying
