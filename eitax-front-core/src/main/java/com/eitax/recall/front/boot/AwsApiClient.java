5988d25779110037cec7f28492c204fc28383338
shokoro <nonamennm03@gmail.com>
2015-11-02 07:02:55 +0900

    1

--- eitax-front-core/src/main/java/com/eitax/recall/front/boot/AwsApiClient.java
+++ eitax-front-core/src/main/java/com/eitax/recall/front/boot/AwsApiClient.java
@@ -1,7 +1,17 @@
 package com.eitax.recall.front.boot;
 
+import java.util.Arrays;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.DescribeInstanceStatusRequest;
+import com.amazonaws.services.ec2.model.DescribeInstanceStatusResult;
+import com.amazonaws.services.ec2.model.InstanceStatus;
+import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
 
 @Component
 public class AwsApiClient implements CommandLineRunner{
@@ -10,6 +20,25 @@ public class AwsApiClient implements CommandLineRunner{
 	public void run(String... arg0) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Hello CLR");
-	}
+        AmazonEC2Client ec2 = Region.getRegion(Regions.AP_NORTHEAST_1)
+                .createClient(AmazonEC2Client.class, null, null);
+        DescribeInstanceStatusRequest req = new DescribeInstanceStatusRequest()
+                .withInstanceIds("i-2fa0068a");
+        DescribeInstanceStatusResult res = ec2.describeInstanceStatus(req);
+        long c = res.getInstanceStatuses()
+        		.stream()
+        		.filter(st -> st.getInstanceState().getName().equals("running"))
+        		.filter(st->st.getInstanceStatus().getStatus().equals("ok"))
+        		.filter(st->st.getSystemStatus().getStatus().equals("ok"))
+        		.count();
+        System.out.println(c);		
+        for (InstanceStatus status : res.getInstanceStatuses()) {
+            System.out.println(status.getInstanceState());
+            System.out.println(status.getInstanceStatus());
+            System.out.println(status.getSystemStatus());
+        }
+        TerminateInstancesRequest terminateRequest = new TerminateInstancesRequest(Arrays.asList("i-2fa0068a"));
+        ec2.terminateInstances(terminateRequest);
+    }
 
 }
