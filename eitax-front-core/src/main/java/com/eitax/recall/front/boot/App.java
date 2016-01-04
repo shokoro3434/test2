756f3e6af50ded97aafedab255493d20a1d23679
shokoro <nonamennm03@gmail.com>
2016-01-05 00:39:55 +0900

    a

--- eitax-front-core/src/main/java/com/eitax/recall/front/boot/App.java
+++ eitax-front-core/src/main/java/com/eitax/recall/front/boot/App.java
@@ -1,27 +1,23 @@
 package com.eitax.recall.front.boot;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.eitax.recall.front.dao.CustomerDAO;
 
-@RestController
+//@RestController
 @EnableAutoConfiguration
-//@SpringBootApplication
+@SpringBootApplication
 @ComponentScan(basePackages={
 		"com.eitax.recall.front.boot",
 		"com.eitax.recall.front.dao",
 		"com.eitax.recall.front.service",
 		"com.eitax.recall.front.web",
+		"jp.co.eitan.cloud.itool.api",
+		"jp.co.eitan.cloud.itool.api.model",
 })
 @EnableJpaRepositories(basePackages={
 		"com.eitax.recall.front.repository",
@@ -30,13 +26,6 @@ import com.eitax.recall.front.dao.CustomerDAO;
 		"com.eitax.recall.front.domain",
 })
 public class App {
-	@Autowired
-	private CustomerDAO customerDAO;
-	@RequestMapping("/")
-	private String home(){
-		customerDAO.findAll().forEach(System.out::println);;
-		return "Hello World hoge";
-	}
 	public static void main(String [] args){
 		SpringApplication.run(App.class, args);
 	}
